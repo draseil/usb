899d40cb9a784cf581a05b3894f063dc93c5b099
Lorenzo <makarafreak@gmail.com>
2015-03-16 11:42:14 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <libusb-1.0/libusb.h>
+
+/* This program was written by Luigi Lorenzo Martinesi
+ * If some asshole tries to steal it, I have given you the right to punch them in the face
+ * This is licensed under the WTFPL:
+ *
+ *         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                     Version 2, December 2004
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+void getDeviceInfo(libusb_device * device, int deviceNumber) {
+	struct libusb_device_descriptor descriptor;
+	libusb_get_device_descriptor(device, &descriptor);
+
+	printf("Device %d:\n", deviceNumber);
+	printf("Vendor ID: %X\n", descriptor.idVendor);
+	printf("Product ID: %X\n", descriptor.idProduct);
+	printf("USB release number: %X\n", descriptor.bcdUSB);
+	printf("USB-IF class code: %d\n\n", descriptor.bDeviceClass);
+}
+
+int main(int argc, char ** argv) {
+	/* ONLY WORKS WITH USB.H
+
+	usb_init();									// initialise libusb
+
+	int totalBusses = usb_find_busses();		// find and count all usb busses
+	int totalDevices = usb_find_devices();		// find and count all usb devices
+	struct usb_bus * bus = usb_get_busses();	// get a usb bus
+	struct usb_device * device = bus->devices;
+
+	printf("Number of busses: %d\nNumber of devices: %d\n", totalBusses, totalDevices);
+
+	int i;
+	for (bus = usb_get_busses(); bus; bus = bus->next) {
+		printf("\nBus directory name: %s\n", bus->dirname);
+
+		for (device = bus->devices; device; device = device->next) {
+			printf("Device filename: %s\n", device->filename);
+		}
+	}*/
+
+	libusb_context * context;
+	libusb_device ** list;
+	libusb_init(&context);
+
+	ssize_t deviceList = libusb_get_device_list(context, &list);
+
+	printf("Number of devices: %d\n\n", deviceList);
+
+	int i;
+	for (i = 0; i < deviceList; i++) {
+		getDeviceInfo(list[i], i);
+	}
+
+	libusb_exit(context);
+
+	return 0;
+}
